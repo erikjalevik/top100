bcd5f2deb6e833d8c1c6066d5c277edd06debc1b
Erik Jälevik <erik.jalevik@futurice.com>
2017-01-08 22:02:00 +0100

    Fix misplaced track

--- _src/db/selectChart.sql
+++ _src/db/selectChart.sql
@@ -1,4 +1,4 @@
 .mode csv
 .headers on
-.output ../../_data/2005.csv
-select chart_pos, artist, title, release_year, group_concat(country_id, '/') as country from track t, track_country tc where t.id=tc.track_id and chart=2005 group by t.id order by chart_pos desc;
+.output ../../_data/2016.csv
+select chart_pos, artist, title, release_year, group_concat(country_id, '/') as country from track t, track_country tc where t.id=tc.track_id and chart=2016 group by t.id order by chart_pos desc;
